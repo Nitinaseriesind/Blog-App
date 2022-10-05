e8a362f4551c576754c827c07e76ff4a5289efc1
Nitin Kumar Srivastava <nitin888sri@gmail.com>
2022-10-05 22:13:36 +0530

    Initial commit

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,23 @@
+import React from "react"
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className='para'>
+          <p>© 2022 A Series Industry - All Rights Reserved.</p>
+        </div>
+        <div className='icon'>
+          
+          <i a href="#" className='fab fa-facebook-f'></i>
+          <i a href="#" className='fab fa-instagram'></i>
+          <i a href="#" className='fab fa-twitter'></i>
+          <i a href="#" className='fab fa-youtube'></i>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
